f5d78ee3773f797fb178cd8affee2c1bfd761e6b
Erlimar Silva Campos <erlimar@gmail.com>
2017-10-03 18:04:21 -0300

    Add E5R.Tools.Bit component

---
+++ src/E5R.Tools.Bit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace E5R.Tools.Bit
+{
+    // http://www.tugberkugurlu.com/archive/compiling-c-sharp-code-into-memory-and-executing-it-with-roslyn
+    // https://msdn.microsoft.com/pt-br/magazine/mt808499.aspx
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
