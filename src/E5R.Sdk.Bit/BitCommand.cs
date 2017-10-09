1bdbc590d70621ce9d2ebf73a3b2a4277e3c3de4
Erlimar Silva Campos <erlimar@gmail.com>
2017-10-09 17:47:00 -0300

    Add copyright header

--- src/E5R.Sdk.Bit/BitCommand.cs
+++ src/E5R.Sdk.Bit/BitCommand.cs
@@ -1,4 +1,7 @@
-﻿namespace E5R.Sdk.Bit
+﻿// Copyright (c) E5R Development Team. All rights reserved.
+// Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
+
+namespace E5R.Sdk.Bit
 {
     public class BitCommand {}
 }
--- src/E5R.Tools.Bit/Program.cs
+++ src/E5R.Tools.Bit/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) E5R Development Team. All rights reserved.
+// Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
+
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
