9dab973d9de3a6c7d71dc4587678b5112927f4bc
Erlimar Silva Campos <erlimar@gmail.com>
2017-10-09 17:38:52 -0300

    Remove manual scripts

--- build.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo Building [dotnet build src/E5R.Tools.Bit/E5R.Tools.Bit.csproj]
-dotnet build src/E5R.Tools.Bit/E5R.Tools.Bit.csproj
--- run.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo -^> Running [E5R.Tools.Bit.dll]
-echo.
-
-dotnet src/E5R.Tools.Bit/bin/Debug/netcoreapp2.0/E5R.Tools.Bit.dll %*
-
-echo.
-echo -^> ExitCode [%ERRORLEVEL%]
