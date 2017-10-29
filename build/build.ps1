09b888742f22e5cf8f8a2904f0044cdbc4cca3a7
Erlimar Silva Campos <erlimar@gmail.com>
2017-10-29 17:49:39 -0200

    Bugfix PS1 scripts

--- build/build.ps1
+++ build/build.ps1
@@ -4,8 +4,8 @@
 $SDK_INSTALL_DIR = "$pwd\.dotnetsdk"
 $DOTNET_EXE ="$SDK_INSTALL_DIR\dotnet.exe"
 
-$DOTNET_EXE --info
-$DOTNET_EXE restore --no-cache
-$DOTNET_EXE build -c $env:CONFIGURATION
+& $DOTNET_EXE --info
+& $DOTNET_EXE restore --no-cache
+& $DOTNET_EXE build -c $env:CONFIGURATION
 
-#$DOTNET_EXE pack ".\src\E5R.Sdk.Bit\E5R.Sdk.Bit.csproj" -c $env:CONFIGURATION /p:VersionPrefix="$env:APPVEYOR_BUILD_VERSION"
+#& $DOTNET_EXE pack ".\src\E5R.Sdk.Bit\E5R.Sdk.Bit.csproj" -c $env:CONFIGURATION /p:VersionPrefix="$env:APPVEYOR_BUILD_VERSION"
