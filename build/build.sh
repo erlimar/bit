3b7c34a2b62a240dd728dfc35bdb0c6286abe304
Erlimar Silva Campos <erlimar@gmail.com>
2017-10-30 11:48:15 -0200

    Implements TravisCI build scripts

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
 # Copyright (c) E5R Development Team. All rights reserved.
 # Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
 
+set -e
+
+SDK_INSTALL_DIR = "$PWD/.dotnetsdk"
+DOTNET_EXE = "$SDK_INSTALL_DIR/dotnet"
+PACKAGE_VERSION = "$VERSION-dev-b-$TRAVIS_BUILD_NUMBER"
+
+"$DOTNET_EXE" --info
+"$DOTNET_EXE" restore --no-cache
+"$DOTNET_EXE" build -c $CONFIGURATION
+
+"$DOTNET_EXE" pack "./src/E5R.Sdk.Bit/E5R.Sdk.Bit.csproj" -c $CONFIGURATION /p:VersionPrefix="$PACKAGE_VERSION"
--- build/test.sh
+++ build/test.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
 # Copyright (c) E5R Development Team. All rights reserved.
 # Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
 
+set -e
+
+SDK_INSTALL_DIR = "$PWD/.dotnetsdk"
+DOTNET_EXE ="$SDK_INSTALL_DIR/dotnet"
+
+"$DOTNET_EXE" test "./test/E5R.Sdk.Bit.Test/E5R.Sdk.Bit.Test.csproj"
+"$DOTNET_EXE" test "./test/E5R.Tools.Bit.Engine.Test/E5R.Tools.Bit.Engine.Test.csproj"
+"$DOTNET_EXE" test "./test/E5R.Tools.Bit.Test/E5R.Tools.Bit.Test.csproj"
