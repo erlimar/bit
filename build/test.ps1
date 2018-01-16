2f0aa31d45e2ae7ae1f65d9643e3c67b2bc5a64b
Erlimar Silva Campos <erlimar@gmail.com>
2018-01-16 15:29:29 -0200

    Removes old build scripts

--- build/test.ps1
+++
@@ -1,9 +0,0 @@
-# Copyright (c) E5R Development Team. All rights reserved.
-# Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
-
-$SDK_INSTALL_DIR = "$pwd\.dotnetsdk"
-$DOTNET_EXE ="$SDK_INSTALL_DIR\dotnet.exe"
-
-& $DOTNET_EXE test ".\test\E5R.Sdk.Bit.Test\E5R.Sdk.Bit.Test.csproj"
-& $DOTNET_EXE test ".\test\E5R.Tools.Bit.Engine.Test\E5R.Tools.Bit.Engine.Test.csproj"
-& $DOTNET_EXE test ".\test\E5R.Tools.Bit.Test\E5R.Tools.Bit.Test.csproj"
